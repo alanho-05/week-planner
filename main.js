16943dcc25506963e01a46709ebd618e2f9fcfad
Kyle Foral <kyleforal@gmail.com>
2023-04-14 18:40:44 +0000

    added css

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ $button.addEventListener('click', function () {
 
 }
 );
+
+/* test */
+
+const popup = document.querySelector('#my-button');
+
+const shut = document.querySelector('#no-button');
+
+const background = document.querySelector('.row');
+
+const shader = document.querySelector('.column-full');
+
+popup.addEventListener('click', popp);
+
+shut.addEventListener('click', closedpop);
+
+function popp(event) {
+  shader.setAttribute('class', 'dark');
+  background.setAttribute('class', 'show');
+}
+
+function closedpop(event) {
+  shader.setAttribute('class', 'overlay');
+  background.setAttribute('class', 'noshow');
+}
