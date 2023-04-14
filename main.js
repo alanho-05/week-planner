1747341c15f7dc6dae89f468278048085f53eefd
Kyle Foral <kyleforal@gmail.com>
2023-04-14 22:01:51 +0000

    added data.js and function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-/* test */
+const $form = document.querySelector('.form');
+
 const $popup = document.querySelector('#my-button');
 
 const shut = document.querySelector('#no-button');
@@ -21,3 +22,15 @@ function closedpop(event) {
   shader.setAttribute('class', 'overlay');
   background.setAttribute('class', 'noshow');
 }
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const entry = {
+    entryId: data.nextEntryId,
+    weekday: event.target.elements.weekday.value,
+    time: event.target.elements.time.value,
+    textarea: event.target.elements.textarea.value
+  };
+  data.entries.shift(entry);
+});
+console.log(entry);
